45f1c96c76d5e984320c7ec238d36b6cce45b036
Fahmy <fahmyfauzii@gmail.com>
2023-12-22 09:21:40 +0700

    chore: events

---
+++ events/index.js
@@ -0,0 +1,31 @@
+// Node js global object
+// run REPL: Object.getOwnPropertyNames(global)
+
+const { EventEmitter } = require('events');
+const moment = require('moment');
+
+const myEventEmitter = new EventEmitter();
+
+// fungsi yang akan dijalankan ketika event coffee-order terjadi
+const makeCoffee = ({ name }) => {
+  const date = moment().format('MMM Do YY');
+  console.log(`Kopi ${name} telah dibuat pada tanggal ${date}`);
+};
+
+const makeBill = ({ price }) => {
+  const date = moment().format('MMM Do YY');
+  console.log(`Bill pada tanggal ${date} sebesar ${price}`);
+};
+
+const payCoffee = ({ price }) => {
+  const date = moment().format('MMM Do YY');
+  console.log(`Coffee telah dibayar sebesar ${price} pada tanggal ${date}`);
+};
+// mendaftarkan fungsi makeCoffee sebagai listener event coffee-order
+myEventEmitter.on('coffee-order', makeCoffee);
+myEventEmitter.on('coffee-order', makeBill);
+myEventEmitter.on('pay-order', payCoffee);
+
+// Memicu event 'coffee-order' terjadi.
+myEventEmitter.emit('coffee-order', { name: 'Tubruk', price: 15000 });
+myEventEmitter.emit('pay-order', { price: 15000 });
